74bf87d6c797c6c867c52f0fa2787d2f92bc6ff8
sparisse <simon.parisse@taztag.com>
2016-05-11 16:52:48 +0200

    Class Matrice, Feature, Product. First implementation

--- OpenCompare/src/main/java/org/opencompare/objects/Feature.java
+++ OpenCompare/src/main/java/org/opencompare/objects/Feature.java
@@ -2,4 +2,33 @@ package org.opencompare.objects;
 
 public class Feature {
 
+	private String name;
+	private String value;
+
+	public Feature() {
+		super();
+	}
+
+	public Feature(String name, String value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
--- OpenCompare/src/main/java/org/opencompare/objects/Matrice.java
+++ OpenCompare/src/main/java/org/opencompare/objects/Matrice.java
@@ -1,5 +1,39 @@
 package org.opencompare.objects;
 
+import java.util.List;
+
 public class Matrice {
+
+	private String name;
+	private List<Product> listProducts;
+
+	public Matrice() {
+		super();
+	}
+
+	public Matrice(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Product> getListProducts() {
+		return listProducts;
+	}
+
+	public void setListProducts(List<Product> listProducts) {
+		this.listProducts = listProducts;
+	}
+
+	public void addProductToList(Product productToAdd) {
+		this.listProducts.add(productToAdd);
+	}
 
 }
