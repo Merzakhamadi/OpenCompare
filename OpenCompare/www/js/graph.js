b2f518bbc4bc3e58b2c7d461211856068bdc34b7
sparisse <simon.parisse@taztag.com>
2016-05-18 12:12:40 +0200

    first commit graph.js style.js

--- OpenCompare/www/js/graph.js
+++ OpenCompare/www/js/graph.js
@@ -7,10 +7,17 @@ nv.utils.symbolMap.set('thin-x', function(size) {
             'm0,' + -(size) +
             'l' + (-size) + ',' + size;
 });
-function loadJSON(callback) {
+
+
+
+//var $svg = $('#graph svg');
+//$svg.parent().append('<div class="col-lg-6">Ceci est un test 2</div>');
+
+function loadJSON(callback, filePath) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'json/matrice.json', false); // Replace 'my_data' with the path to your file
+  xobj.open('GET', filePath, false);
+  //xobj.open('GET', 'json/matrice.json', false); // Replace 'my_data' with the path to your file
   xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -20,7 +27,7 @@ function loadJSON(callback) {
   xobj.send(null);
 }
 // create the chart
-var srcdata;
+var dataParam;
 var chart;
 
 nv.addGraph(function() {
@@ -30,25 +37,46 @@ nv.addGraph(function() {
         .useVoronoi(true)
         .color(d3.scale.category10().range())
         .duration(300)
+        .showLegend(false)
     ;
+
     chart.dispatch.on('renderEnd', function(){
         console.log('render complete');
     });
 
     chart.xAxis.tickFormat(d3.format('.02f'));
     chart.yAxis.tickFormat(d3.format('.02f'));
-    chart.xAxis.axisLabel('test1');
-    chart.yAxis.axisLabel('test2');
-    //dataa = JSON.parse(data1);
+
+    console.log("processing graph parameters ");
+    loadJSON(function(response) {
+
+        var obj = JSON.parse(response)
+        //console.log(obj);
+        chart.xAxis.axisLabel(obj.labelx);
+        chart.yAxis.axisLabel(obj.labely);
+        var sp1 = document.createElement("h2");
+        // ajout du titre du gaph dans fichier html
+        // crée un peu de contenu pour cet élément.
+        var sp1_content = document.createTextNode(obj.titre);
+        // ajoute ce contenu au nouvel élément
+        sp1.appendChild(sp1_content);
+        // lui donne un attribut id appelé 'nouveauSpan'
+        var sp2 = document.getElementById("graphtitle");
+        // Obtient une référence du nœud parent
+        var parentDiv = sp2.parentNode;
+        // insère le nouvel élément dans le DOM avant sp2
+        parentDiv.insertBefore(sp1, sp2);
+      },'json/param.json');
+
     console.log("processing DATA and plot:: ");
 
     loadJSON(function(response) {
-     // Parse JSON string into object
+
      d3.select('#graph svg')
          //.datum(dataa)
          .datum(JSON.parse(response))
          .call(chart);
-     });
+     },'json/matrice.json');
 
     //randomData(1,20);
     //console.log(JSON.stringify(dataa));
